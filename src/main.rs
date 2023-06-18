22f00dae2bcfd4ac810855ce51def69a4864ef66
Blastbrean <73199280+Blastbrean@users.noreply.github.com>
2023-06-17 23:33:37 -0700

    Fixed key-automation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Write, Read};
+use std::io::{self, Read, Write};
 
 fn pause() {
     let mut stdout = io::stdout();
@@ -19,8 +19,8 @@ async fn main() -> Result<(), reqwest::Error> {
 
     // attempt to read line...
     match io::stdin().read_line(&mut buffer) {
-        Ok(_line) => {},
-        Err(err) => println!("failed to get read-line: {}", err)
+        Ok(_line) => {}
+        Err(err) => println!("failed to get read-line: {}", err),
     }
 
     // parse response...
@@ -29,24 +29,77 @@ async fn main() -> Result<(), reqwest::Error> {
         hwid => format!("automating with hwid: {hwid}"),
     };
 
-    // set parameters...
+    // ok, we start from one...
     let params = [("updated_browser", "true"), ("HWID", &response)];
+    client
+        .post("https://flux.li/windows/start.php?updated_browser=true")
+        .form(&params)
+        .send()
+        .await?;
+
+    // check 1...
+    let res_check_one = client.post("https://fluxteam.net/windows/checkpoint/check1.php")
+           .form(&params)
+           .header("Referer", "https://linkvertise.com/")
+           .header("Sec-Fetch-Site", "cross-site")
+           .header("Sec-Fetch-Mode", "navigate")
+           .header("Sec-Fetch-Dest", "document")
+           .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+           .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+           .send()
+           .await?
+           .text()
+           .await?;
+
+    if res_check_one
+        == "Trying to bypass the Fluxus key system will get you banned from using Fluxus."
+    {
+        println!("failed to bypass res_check_two...");
+        pause();
+    } else {
+        println!("bypassed res_check_one..");
+    }
+
+    // check 2...
+    let res_check_two = client.post("https://fluxteam.net/windows/checkpoint/check2.php")
+           .form(&params)
+           .header("Referer", "https://linkvertise.com/")
+           .header("Sec-Fetch-Site", "cross-site")
+           .header("Sec-Fetch-Mode", "navigate")
+           .header("Sec-Fetch-Dest", "document")
+           .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+           .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+           .send()
+           .await?
+           .text()
+           .await?;
+
+    if res_check_two
+        == "Trying to bypass the Fluxus key system will get you banned from using Fluxus."
+    {
+        println!("failed to bypass res_check_two...");
+        pause();
+    } else {
+        println!("bypassed res_check_two..");
+    }
 
     // check final...
     let res_check_final = client.post("https://fluxteam.net/windows/checkpoint/main.php")
-        .form(&params)
-        .header("Referer", "https://linkvertise.com/")
-        .header("Sec-Fetch-Site", "cross-site")
-        .header("Sec-Fetch-Mode", "navigate")
-        .header("Sec-Fetch-Dest", "document")
-        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
-        .send()
-        .await?
-        .text()
-        .await?;
+           .form(&params)
+           .header("Referer", "https://linkvertise.com/")
+           .header("Sec-Fetch-Site", "cross-site")
+           .header("Sec-Fetch-Mode", "navigate")
+           .header("Sec-Fetch-Dest", "document")
+           .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+           .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+           .send()
+           .await?
+           .text()
+           .await?;
 
-    if res_check_final == "Trying to bypass the Fluxus key system will get you banned from using Fluxus." {
+    if res_check_final
+        == "Trying to bypass the Fluxus key system will get you banned from using Fluxus."
+    {
         println!("failed to bypass res_check_final...");
         pause();
     } else {
@@ -60,21 +113,28 @@ async fn main() -> Result<(), reqwest::Error> {
     for line in res_check_final.lines() {
         // find "let content" (contains the key...)
         let key_in_this_line = line.contains("let content");
-        if !key_in_this_line
-        {
-            continue
+        if !key_in_this_line {
+            continue;
         }
 
         // print key...
-        println!("here is your key: {}", line.replace("let content = (\"", "").replace("\");", "").trim());
+        println!(
+            "here is your key: {}",
+            line.replace("let content = (\"", "")
+                .replace("\");", "")
+                .trim()
+        );
         pause();
 
         // break out of loop by returning...
-        return Ok(())
+        return Ok(());
     }
 
     // failed to find key...
-    println!("failed to find key, report this output to blastbrean on discord: {}", res_check_final);
+    println!(
+        "failed to find key, report this output to blastbrean on discord: {} -> {}",
+        res_check_one, res_check_final
+    );
     pause();
 
     // finish execution...
